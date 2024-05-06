70523b01881199ea5c54ea2b8f70092cb4dc3d8b
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2024-05-06 23:28:52 +0700

    mongo connect

---
+++ src/config/mongodb.js
@@ -0,0 +1,8 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+//FbrQ7jKE5b9yDdvW
+//nvvinvla12019
+const MONGODB_URI = 'mongodb+srv://nvvinvla12019:FbrQ7jKE5b9yDdvW@cluster0-nguyenvanvi.3c1iq1n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-NguyenVanVi'
