a1cc0a00058e597c61bed4d68e3534bb62ff39b3
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2024-05-07 17:39:35 +0700

    config env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -5,13 +5,12 @@
  */
 //JNaHuxI7QU0huI0I
 //nvvinvla12019
-const MONGODB_URI = 'mongodb+srv://nvvinvla12019:JNaHuxI7QU0huI0I@cluster0.cfrkmbp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const DATABASE_NAME = 'trello-mern-stack-pro'
 
 import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from './environment'
 let trelloDatabaseInstance = null
 // khoi tao doi tuong mongodbClientInstance de connect toi mongodb
-const mongodbClientInstance = new MongoClient(MONGODB_URI,{
+const mongodbClientInstance = new MongoClient(env.MONGODB_URI, {
     serverApi:{
         version:ServerApiVersion.v1,
         strict:true,
@@ -22,7 +21,7 @@ export const CONNECT_DB  =async ()=>{
     // goi ket noi toi mongodb atlats voi url da khai bao trong than cua mongodbClientInstance
     await mongodbClientInstance.connect()
     //ket noi thanh cong thi lay ra database theo ten va gan nguoc lai vao bien trelloDatabaseInstance
-    trelloDatabaseInstance = mongodbClientInstance.db(DATABASE_NAME)
+    trelloDatabaseInstance = mongodbClientInstance.db(env.DATABASE_NAME)
 }
 export const GET_DB = ()=>{
     if(!trelloDatabaseInstance) throw new Error('must connect to database first')
--- src/server.js
+++ src/server.js
@@ -7,18 +7,15 @@
 import express from 'express'
 import { CONNECT_DB,GET_DB,CLOSE_DB } from './config/mongodb'
 import exitHook from 'async-exit-hook'
+import { env } from './config/environment'
 const START_SERVER = ()=>{
   const app = express()
-
-  const hostname = 'localhost'
-  const port = 8017
-
   app.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1><hr>')
   })
 
-  app.listen(port, hostname, () => {
-    console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello Trung Quan Dev, I am running at ${ env.APP_HOST }:${ env.APP_PORT }/`)
   })
   // thực hiện các tác vụ clean up trước khi dừng server
   exitHook(()=>{
